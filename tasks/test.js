3086e7b7f942864321e69cb7d527e7098be786be
Yu Isoda <isoda@maboroshi.biz>
2016-06-23 10:57:30 +0900

    chore: テスト環境にbrowsersyncとlivereloadを導入した

--- tasks/test.js
+++ tasks/test.js
@@ -1,14 +1,23 @@
 'use strict';
 
-const path    = require('path'); 
-const gulp    = require('gulp');
-const qunit   = require('gulp-qunit');
-const plumber = require('gulp-plumber');
+const path        = require('path'); 
+const gulp        = require('gulp');
+const qunit       = require('gulp-qunit');
+const plumber     = require('gulp-plumber');
+const browserSync = require('browser-sync');
+
+// HTML確認用にローカルサーバーを起動する
+browserSync({
+  ui       : false,
+  server   : path.join(__dirname, '..'),
+  startPath: '../test/index.html'
+});
 
 module.exports = function () {
   return gulp.src(path.join(__dirname, '..', 'test/index.html'))
     .pipe(plumber())
-    .pipe(qunit());
+    .pipe(qunit())
+    .pipe(browserSync.reload({stream: true}));
 };
 
 module.exports.watch = function () {
